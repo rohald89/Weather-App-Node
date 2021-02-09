de1e9a615b0a8a85984afe1cad76e29023244e5a
Rohald van Merode <rohald_89@live.nl>
2021-02-09 13:30:29 +0100

    added header / footer partials and added some basic styling

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 // https://expressjs.com/
 const express = require('express');  
 
+const hbs = require('hbs');
+
 
 
 // https://expressjs.com/en/4x/api.html#express
@@ -12,7 +14,8 @@ const app = express();
 //Define paths for Express config
     //  https://nodejs.org/dist/latest-v14.x/docs/api/path.html#path_path_join_paths
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');  // by default its looking for a "views"-directory when renaming or relocating it, you have to define its name and location
+const viewsPath = path.join(__dirname, '../templates/views');  // by default its looking for a "views"-directory when renaming or relocating it, you have to define its name and location
+const partialsPath = path.join(__dirname, '../templates/partials');  // create a path to the partials to register to handlebars
 
 
 // Setup Handlebars engine and views location
@@ -21,6 +24,7 @@ const viewsPath = path.join(__dirname, '../templates');  // by default its looki
     //  https://expressjs.com/en/guide/using-template-engines.html
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);  // make sure the views get loaded by defining the directory where they're located. this wouldn't have been needed if the directory was called "views" + located in project root folder.
+hbs.registerPartials(partialsPath); // lets handlebars know where to locate the partials
 
 // Setup static directory to serve
     //  https://expressjs.com/en/4x/api.html#app.use
@@ -43,6 +47,8 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {  
     res.render('help', {
+        title: 'Help',
+        name: 'Rohald van Merode',
         message: 'This is some helpful text'
     });
 });
@@ -52,6 +58,22 @@ app.get('/weather', (req, res) => {
         forecast: "rainy",
         location: 'Amsterdam'
     });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Rohald van Merode',
+        error: "Help article not found"
+    })
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Rohald van Merode',
+        error: "Page not found"
+})
 });
 
 
