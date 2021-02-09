91cc4ef338bd55486ce1c5ab78af3d5d92fdef10
Rohald van Merode <rohald_89@live.nl>
2021-02-09 12:17:38 +0100

    added handlebars as view engine, created templates to replace the static html files

--- src/app.js
+++ src/app.js
@@ -9,13 +9,42 @@ const express = require('express');
 // Creates an Express application. The express() function is a top-level function exported by the express module.
 const app = express();  
 
-//  https://expressjs.com/en/4x/api.html#app.use
-//  https://nodejs.org/dist/latest-v14.x/docs/api/path.html#path_path_join_paths
-app.use(express.static(path.join(__dirname, '../public')))
+//Define paths for Express config
+    //  https://nodejs.org/dist/latest-v14.x/docs/api/path.html#path_path_join_paths
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');  // by default its looking for a "views"-directory when renaming or relocating it, you have to define its name and location
+
+
+// Setup Handlebars engine and views location
+    //  https://expressjs.com/en/4x/api.html#app.set
+    //  hbs = express view engine for handlebars   // https://www.npmjs.com/package/hbs
+    //  https://expressjs.com/en/guide/using-template-engines.html
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);  // make sure the views get loaded by defining the directory where they're located. this wouldn't have been needed if the directory was called "views" + located in project root folder.
+
+// Setup static directory to serve
+    //  https://expressjs.com/en/4x/api.html#app.use
+app.use(express.static(publicDirectoryPath));
 
 //  https://expressjs.com/en/4x/api.html#app.get
 app.get('', (req, res) => {  
-    res.send('<h1>Weather</h1>');
+    res.render('index', {
+        title: 'Weather',
+        name: 'Rohald van Merode'
+    });
+});
+
+app.get('/about', (req, res) => {  
+    res.render('about', {
+        title: 'About Me',
+        name: 'Rohald van Merode'
+    });
+});
+
+app.get('/help', (req, res) => {  
+    res.render('help', {
+        message: 'This is some helpful text'
+    });
 });
 
 app.get('/weather', (req, res) => {
